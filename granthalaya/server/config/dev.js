c85ef33e77414fbf19dabe9583f5660e247bafc2
Om-V-Patel <105061098+Om-V-Patel@users.noreply.github.com>
2022-07-08 23:02:32 +0530

    Delete dev.js

--- granthalaya/server/config/dev.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    MONGOURI: 'mongodb+srv://omvpatel:9876@cluster0.ewvcf.mongodb.net/?retryWrites=true&w=majority',
-    JWT_SECRET: "granthalaya",
-    SENDGRID_API:"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",
-  
-  };
